98796f6d970921750b5e2e6f4a7415ae1559dfdb
Muhammad Talha <96860817+muhammadtalha198@users.noreply.github.com>
2023-08-28 11:31:02 +0500

    Update app.js

--- JavaScript-Refresher/app.js
+++ JavaScript-Refresher/app.js
@@ -1,1 +1,10 @@
 export let apiKey = "https/muhmma/api";
+export let apiKey0 = "https/muhmma/api";
+export let apiKey1 = "https/muhmma/api";
+export let apiKey2 = "https/muhmma/api";
+
+/* 
+    you can just export this only from the whole file anyother export
+    will not work in default export case. 
+*/
+export default "https/muhmma/api";
