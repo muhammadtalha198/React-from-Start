7dccfcabbb10cda946df1102cb4b4fd0b5d57bda
Muhammad Talha <96860817+muhammadtalha198@users.noreply.github.com>
2023-08-28 12:30:46 +0500

    Create arroeFunctions.js

---
+++ JavaScript-Refresher/arroeFunctions.js
@@ -0,0 +1,33 @@
+/**
+ * its also called a anonymous function because it doesent have a name.
+ */
+
+/*
+    define like this;
+*/
+
+export default () => {
+	console.log("Hello world");
+};
+
+/*
+    define also like this with parameters;
+
+
+export default (userName,message) => {
+    console.log('Hello world');
+    return userName + ' ' + message;
+};
+
+*/
+
+/*
+    define also like this;
+*/
+
+/**
+export default function() {
+    console.log('Hello world');
+ };
+
+ */
