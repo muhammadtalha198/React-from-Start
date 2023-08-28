7e33cc41c9a401190406bc000e11da9e65c102ed
Muhammad Talha <96860817+muhammadtalha198@users.noreply.github.com>
2023-08-28 14:51:43 +0500

    Update arrays.js

--- JavaScript-Refresher/arrays.js
+++ JavaScript-Refresher/arrays.js
@@ -1,4 +1,4 @@
- const hobbies = ["Sports0", "Sports1", "Sports2", "Sports3"];
+const hobbies = ["Sports0", "Sports1", "Sports2", "Sports3"];
 
 console.log(hobbies);
 console.log(hobbies[0]);
@@ -7,6 +7,8 @@ console.log(hobbies[1]);
 hobbies.push("Learning");
 console.log(hobbies);
 console.log(hobbies[4]);
+
+//----------------------------------------------------------------
 
 const index = hobbies.findIndex((item) => {
 	return item === "Sports2";
@@ -21,3 +23,35 @@ console.log(index);
 const index1 = hobbies.findIndex((item) => item === "Sports2");
 
 console.log(index);
+
+//----------------------------------------------------------------
+
+const edititedHobies = hobbies.map((item) => {
+	return item + "!";
+});
+
+console.log(edititedHobies);
+
+/**
+ * shortest way of writhing upper code :
+ */
+
+const index2 = hobbies.map((item) => item + "!");
+
+console.log(index2);
+
+//----------------------------------------------------------------
+
+const objects = hobbies.map((item) => {
+	return { item: item };
+});
+
+console.log(objects);
+
+/**
+ * shortest way of writhing upper code :
+ */
+
+const objects1 = hobbies.map((item) => ({ item: item }));
+
+console.log(objects1);
