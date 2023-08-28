1e165098a8f4a1e711e76ee9d8a36324ff70cee1
Muhammad Talha <96860817+muhammadtalha198@users.noreply.github.com>
2023-08-28 12:30:19 +0500

    Create functions.js

---
+++ JavaScript-Refresher/functions.js
@@ -0,0 +1,13 @@
+function greetings0() {
+	console.log("Hello");
+}
+
+greetings0();
+
+function greetings(userName, greeting) {
+	console.log(userName);
+	console.log(greeting);
+}
+
+greetings("Muhammad", "World");
+ 
