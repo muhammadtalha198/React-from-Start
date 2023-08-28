2b3ca83f0541e0f5de04f540ecf792d27bb9520c
Muhammad Talha <96860817+muhammadtalha198@users.noreply.github.com>
2023-08-28 11:30:31 +0500

    Update import.js

--- JavaScript-Refresher/import.js
+++ JavaScript-Refresher/import.js
@@ -1,3 +1,31 @@
+/*
+    in case of single import...
+*/
+
 import { apiKey } from "./app";
 
+//----------------------------------------------------------------
+
+/*
+    in case of multiple import...
+*/
+import { apiKey0, apiKey1, apiKey2 } from "./app";
+
+//----------------------------------------------------------------
+
+/*
+ import all exports as an object by putting any name.
+*/
+
+import * as allApiKeys from "/app";
+
+console.log(allApiKeys.apiKey, allApiKeys.apiKey1);
+
+//----------------------------------------------------------------
+
+/*
+in default export case:use any name to import.
+*/
+
+import anyNameApiKey from "./app";
 console.log(apiKey);
