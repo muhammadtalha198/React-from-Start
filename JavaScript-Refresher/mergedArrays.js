5a08d2068e6c0d415b38697dccabc966f837f230
Muhammad Talha <96860817+muhammadtalha198@users.noreply.github.com>
2023-08-29 12:21:25 +0500

    Create mergedArrays.js

---
+++ JavaScript-Refresher/mergedArrays.js
@@ -0,0 +1,9 @@
+const hobbies = ["Sports0", "Sports1", "Sports2", "Sports3"];
+
+const newArray = ["Bike", "Cycle"];
+
+const mergedArray1 = [...hobbies];
+const mergedArray2 = [...hobbies, ...newArray];
+
+console.log(mergedArray1);
+console.log(mergedArray2);
