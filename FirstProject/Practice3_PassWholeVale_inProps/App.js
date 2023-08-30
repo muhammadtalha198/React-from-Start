eaad68389eed0e8bd0b02da19da55a95aa81d80b
Muhammad Talha <96860817+muhammadtalha198@users.noreply.github.com>
2023-08-30 15:52:52 +0500

    Update App.js

--- FirstProject/Practice3_PassWholeVale_inProps/App.js
+++ FirstProject/Practice3_PassWholeVale_inProps/App.js
@@ -44,13 +44,19 @@ function App() {
 
 
 			<ExpenseItem1
-				expense={expenses[0]}
+				title={expenses[0].title}
+				amount={expenses[0].amount}
+				date={expenses[0].date}
 			/>
 			<ExpenseItem1
-				expense={expenses[1]}
+				title={expenses[1].title}
+				amount={expenses[1].amount}
+				date={expenses[1].date}
 			/>
 			<ExpenseItem1
-				expense={expenses[2]}
+				title={expenses[2].title}
+				amount={expenses[2].amount}
+				date={expenses[2].date}
 			/>
 
 			
