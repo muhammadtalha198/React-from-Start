309fdb5d954f7e484d11577946256b8c237c7a99
Muhammad Talha <96860817+muhammadtalha198@users.noreply.github.com>
2023-08-30 14:09:55 +0500

    Update ExpenseItem.js

--- FirstProject/Practice2/ExpenseItem.js
+++ FirstProject/Practice2/ExpenseItem.js
@@ -1,16 +1,20 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-	let itemDate = new Date(2023, 8, 29); //builtin function Date();
-	let itemName = "Car Insurance";
-	let itemPrice = 345.87;
+function ExpenseItem(props) {
+	
+    /*
+        let itemDate = new Date(2023, 8, 29); //builtin function Date();
+        let itemName = "Car Insurance";
+        let itemPrice = 345.87;
 
-	return (
+        Now we set all those upper static values with props from the main component.
+	*/
+    return (
 		<div className='expense-item'>
-			<div>{itemDate.toISOString()}</div>
+			<div>{props.date.toISOString()}</div>
 			<div className='expense-item__description'>
-				<h2 className='expense-item h2'>{itemName}</h2>
-				<div className='expense-item__price'>{itemPrice}</div>
+				<h2 className='expense-item h2'>{props.title}</h2>
+				<div className='expense-item__price'>{props.amount}</div>
 			</div>
 		</div>
 	);
